1a027a921fb6778f8c09e5cd7733805638270188
DesiganDeva <rmdesigan0303@gmail.com>
2024-04-01 18:35:16 +0530

    frog

--- Python/HW/frogsList.py
+++ Python/HW/frogsList.py
@@ -9,27 +9,33 @@
 # 
 # Imagine the frog becomes bigger as much as the size of the frog it ate. How would it change the program
 # 
-# 
-# 
+# in -> 1,2,3,4,5,6,7
+# out -> 21,7
 
 # build a function for find alive frog's
 def findAliveFrogs(frog_list):
     # assign alive_frog_list as first frog size in frog_list
     alive_frog_list = [0]
-    # get all frog's from frog_list one by one
-    for index in range(len(frog_list)):
-        # check the current frog size is graterthan the last frog size in alive_frog_list
-        if alive_frog_list[-1] >= frog_list[index]:
-            # add the frogin alive_frog_list
-            alive_frog_list.append(frog_list[index])
-        # else
-        else:
-            # remove the frog from alive_frog_list
-            size = alive_frog_list.pop()
-            # add the current frog in alive_frog_list with added added size of removed frog size
-            alive_frog_list.append(frog_list[index]+size)
-    # return alive_frog_list
-    return alive_frog_list
+    # check frog_list is sorted in desc order 
+    if frog_list==sorted(frog_list,reverse=True):
+        # return frog_list
+        return frog_list
+    # else
+    else:
+        # get all frog's from frog_list one by one
+        for index in range(len(frog_list)):
+            # check the current frog size is graterthan the last frog size in alive_frog_list
+            if alive_frog_list[-1] >= frog_list[index]:
+                # add the frogin alive_frog_list
+                alive_frog_list.append(frog_list[index])
+            # else
+            else:
+                # remove the frog from alive_frog_list
+                size = alive_frog_list.pop()
+                # add the current frog in alive_frog_list with added added size of removed frog size
+                alive_frog_list.append(frog_list[index]+size)
+        # return call findAliveFrogs(alive_frog_list)
+        return findAliveFrogs(alive_frog_list)
 
 # get input grof size from user seperated by , and spit into list
 # map the value to input_list in int datatype
